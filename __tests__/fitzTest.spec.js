e00cc34740c81487494b32fb2c1a6194105701bd
Gensen Choy <gensen.choy@gmail.com>
2020-11-09 18:20:40 -0500

    MAIN-4671: Add JSDoc

--- __tests__/fitzTest.spec.js
+++ __tests__/fitzTest.spec.js
@@ -1,4 +1,5 @@
 const fitzjs = require('../fitzjs');
+const moment = require('moment');
 
 describe('Lets fitzTest!!', () => {
     const dateFormat = 'YYYY-MM-DD hh:mm:ss a';
@@ -11,3 +12,18 @@ describe('Lets fitzTest!!', () => {
         expect(utcIsoDate).toBe('2019-11-06T17:39:22.000Z');
     });
 });
+
+describe('Moment formats', () => {
+    test('Should parse MomentBuiltinFormat args', () => {
+        const now = new Date().toISOString();
+        expect(fitzjs(now, moment.ISO_8601)).toBe(now);
+
+    });
+
+    test('Should parse HTML5 format date args', () => {
+        const fitzJzBirthday = '2019-05';
+        const dateString = fitzjs(fitzJzBirthday, moment.HTML5_FMT.MONTH);
+        expect(dateString).toBe('2019-05-01T00:00:00.000Z');
+    });
+});
+
